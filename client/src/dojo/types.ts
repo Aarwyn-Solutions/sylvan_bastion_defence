2ad1cab6ec7303745f4e45069f43e772f7d4f770
nikania <nianemis@gmail.com>
2023-10-21 22:40:13 -0300

    added client

---
+++ client/src/dojo/types.ts
@@ -0,0 +1,29 @@
+export enum HeroType {
+    // None
+    None,
+
+    // Range attack:
+    Archer,
+    Shaman,
+    Mage,
+
+    // Melee atack
+    Knight,
+    Druid,
+    Vendigo,
+    Guardian,
+}
+
+export enum Artifact {
+    None,
+    WoodenBow,
+    ShordSword,
+    WoodenShield,
+}
+
+export enum DungeonType {
+    None,
+    GoblinCamp,
+    WitchTown,
+    BlackTower,
+}
